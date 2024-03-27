e7cf4bc16e75fda36377b76cce38f3744a990dca
AshMW2724 <contact@ashmw.com>
2024-03-27 11:44:18 -0400

    PIP now uses screen context instead of screen width

--- src/components/pictureInPicture.tsx
+++ src/components/pictureInPicture.tsx
@@ -1,5 +1,7 @@
+import useScreen from '@/hooks/useScreen';
 import cn from '@/utils/cn';
 import { ReactNode } from 'react';
+import { ScreenLayout } from './screenProvider';
 
 interface Props {
   children: ReactNode;
@@ -8,9 +10,11 @@ interface Props {
 
 export default function PictureInPicture(props: Props) {
   const { className, children } = props;
+  const { layout } = useScreen();
 
+  if (layout !== ScreenLayout.PIP) return `${layout}`;
   return (
-    <div style={{ zIndex: 999999 }} className={cn('fixed top-0 left-0 right-0 bottom-0 bg-[#242628] pip:hidden', className)}>
+    <div style={{ zIndex: 999999 }} className={cn('fixed top-0 left-0 right-0 bottom-0 bg-[#242628]', className)}>
       {children}
     </div>
   );
